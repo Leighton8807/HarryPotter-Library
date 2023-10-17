368580ca6deb6e46a72a6e7a65ca5ea4e522ef8f
Leighton8807 <joge.8807@gmail.com>
2023-10-17 09:49:41 -0500

    ADD: add navbar init

---
+++ HP-FRONT/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return(
+    <>
+      <h2>HARRY POTTER LIBRARY</h2>
+      <div id='navContainer'>
+        <a  href="#h">Home</a>
+        <a  href="#h">cart</a>
+      </div>
+
+    </>
+  )
+  
+}
+
+export default Navbar;
